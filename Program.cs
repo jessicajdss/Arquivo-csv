343aebb5246f233f6881465440d5c071f29f0b2d
Jéssica Souza <jessica.jdss@gmail.com>
2017-10-11 20:52:51 -0300

    Criação do arquivo csv

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace arquivocsv
 {
@@ -6,7 +7,30 @@ namespace arquivocsv
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome, email;
+            int idade;
+
+            Console.WriteLine("Informe o seu nome: ");
+            nome = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Informe o seu email: ");
+            email = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Informe a sua idade: ");
+            idade = Int16.Parse(Console.ReadLine());
+
+            //usar crtl e . para achar a biblioteca do comando caso você não saiba;
+            // true adiciona as linhas no arquivo já existente, sobrescreve.
+            StreamWriter arquivo = new StreamWriter("dados.csv", true);
+            //if (arquivo.)
+            arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString());
+            
+            
+            arquivo.Close();
+
+
+
+
         }
     }
 }
